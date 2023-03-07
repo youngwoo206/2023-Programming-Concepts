1ed8390ae1af46c8cc336b63be9e943a25dcac25
Youngwoo <youngwoo206@gmail.com>
2023-03-06 22:31:11 -0500

    search rotated sorted array

--- Leetcode/Search Rotated Sorted Array.py
+++ Leetcode/Search Rotated Sorted Array.py
@@ -9,3 +9,28 @@ Given the array nums after the possible rotation and an integer target, return t
 
 You must write an algorithm with O(log n) runtime complexity.
 '''
+
+
+def search(nums, target):
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l+r)//2
+        if target == nums[mid]:
+            return mid
+
+        # left portion
+        if nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        # right portion
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+    return -1
