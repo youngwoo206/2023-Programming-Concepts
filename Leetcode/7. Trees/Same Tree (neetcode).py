f759d407b0635c7d1164d5868dd8d615294771c0
Youngwoo <youngwoo206@gmail.com>
2023-04-27 13:29:26 -0400

    better same tree

---
+++ Leetcode/7. Trees/Same Tree (neetcode).py
@@ -0,0 +1,14 @@
+'''
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+'''
+
+
+def isSameTree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q or p.val != q.val:
+        return False
+
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
