02c4ed87ea7c2db78f7a7690c5bd74699ba83d9f
Youngwoo <youngwoo206@gmail.com>
2023-04-30 18:35:34 -0400

    subtree

---
+++ Leetcode/7. Trees/Subtree.py
@@ -0,0 +1,8 @@
+'''
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of 
+root with the same structure and node values of subRoot and false otherwise
+'''
+
+
+def isSubtree(root, subRoot):
+    pass
